2f85bf6b5217cee5bfa3d36fa0e17560527c7b11
Simon Lagos <simon.lagos.sallhed@gmail.com>
2019-10-13 12:17:44 +0900

    Add unit tests

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+// This file is only needed for tests
+
+module.exports = {
+  presets: ['@babel/preset-env'],
+}
---
+++ test.js
@@ -0,0 +1,60 @@
+import test from 'ava'
+
+import * as ex from './index'
+
+const mockStage = {
+  scale: { set: () => {} },
+  children: [
+    { name: 'child1', children: [{ name: 'grandchild', children: [] }] },
+    { name: 'child2', children: [] },
+  ],
+}
+
+const mockPixiApp = {
+  stage: mockStage,
+  renderer: {
+    width: 800,
+    height: 600,
+    resize: () => {},
+  },
+  loader: {
+    resources: {
+      'spritesheet.json': {
+        textures: { 'sprite1.png': {} },
+      },
+    },
+  },
+}
+
+
+test('some functions throw errors before init is called', (t) => {
+  t.throws(ex.getTexture)
+  t.throws(ex.getAllTextureIds)
+  t.throws(ex.drawHitArea)
+  t.throws(ex.resize)
+  ex.init(mockPixiApp)
+})
+
+test('getTexture', (t) => {
+  t.deepEqual(ex.getTexture('sprite1'), {})
+})
+
+test('getAllTextureIds', (t) => {
+  t.deepEqual(ex.getAllTextureIds(), ['sprite1'])
+})
+
+test('getAllChildren', (t) => {
+  t.deepEqual(
+    ex.getAllChildren(mockStage),
+    [
+      mockStage.children[0].children[0],
+      mockStage.children[0],
+      mockStage.children[1],
+      mockStage,
+    ],
+  )
+})
+
+test('resize', (t) => {
+  t.notThrows(() => { ex.resize(1200, 1000) })
+})

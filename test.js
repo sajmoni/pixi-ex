0ec6a9e4c215b65a6893d1445e2aecc826ee128e
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-04-16 09:58:30 +0900

    Add tests

--- test.js
+++ test.js
@@ -62,6 +62,22 @@ test('resize', (t) => {
   t.notThrows(() => { ex.resize(1200, 1000) })
 })
 
+test('centerX', (t) => {
+  const displayObject = { width: 100, x: 0, pivot: { x: 0 } }
+  const xPosition = 500
+  ex.centerX(displayObject, xPosition)
+  t.is(displayObject.x, 500)
+  t.is(displayObject.pivot.x, 50)
+})
+
+test('centerY', (t) => {
+  const displayObject = { height: 100, y: 0, pivot: { y: 0 } }
+  const yPosition = 500
+  ex.centerY(displayObject, yPosition)
+  t.is(displayObject.y, 500)
+  t.is(displayObject.pivot.y, 50)
+})
+
 test('init - duplicate texture names across sprite sheets', (t) => {
   t.deepEqual(ex.getTexture('sprite1'), {})
   t.throws(() => ex.init({

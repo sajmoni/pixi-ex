f0ea3ab4d418c0a33e46a94389c3f72720b24dbc
Simon Lagos <simon.lagos.sallhed@gmail.com>
2022-02-08 22:06:03 +0100

    Format test file

--- test.ts
+++ test.ts
@@ -1,25 +1,25 @@
-import test from "ava";
-import * as PIXI from "pixi.js";
-import * as ex from "pixi-ex";
+import test from 'ava'
+import * as ex from 'pixi-ex'
+import { Container } from 'pixi.js'
 
-import * as internal from "./src/internal";
+import * as internal from './src/internal'
 
-const container = new PIXI.Container();
-container.name = "container";
-const grandChild = new PIXI.Container();
-grandChild.name = "grandChild";
-const child1 = new PIXI.Container();
-child1.name = "child1";
-child1.addChild(grandChild);
-const child2 = new PIXI.Container();
-child2.name = "child2";
+const container = new Container()
+container.name = 'container'
+const grandChild = new Container()
+grandChild.name = 'grandChild'
+const child1 = new Container()
+child1.name = 'child1'
+child1.addChild(grandChild)
+const child2 = new Container()
+child2.name = 'child2'
 
-const mockStage = new PIXI.Container();
-mockStage.name = "stage";
-mockStage.addChild(child1);
-mockStage.addChild(child2);
+const mockStage = new Container()
+mockStage.name = 'stage'
+mockStage.addChild(child1)
+mockStage.addChild(child2)
 
-mockStage.addChild(container);
+mockStage.addChild(container)
 
 const mockPixiApp = {
   stage: mockStage,
@@ -30,112 +30,112 @@ const mockPixiApp = {
   },
   loader: {
     resources: {
-      "spritesheet.json": {
-        textures: { "sprite1.png": {} },
+      'spritesheet.json': {
+        textures: { 'sprite1.png': {} },
       },
     },
   },
-};
+}
 
-test("some functions throw errors before init is called", (t) => {
-  t.throws(() => ex.getTexture);
-  t.throws(() => ex.getAllTextureIds);
-  t.throws(() => ex.drawHitArea);
-  t.throws(() => ex.resize);
-  t.throws(() => ex.showGrid);
+test('some functions throw errors before init is called', (t) => {
+  t.throws(() => ex.getTexture)
+  t.throws(() => ex.getAllTextureIds)
+  t.throws(() => ex.drawHitArea)
+  t.throws(() => ex.resize)
+  t.throws(() => ex.showGrid)
   //@ts-expect-error
-  ex.init(mockPixiApp);
-});
+  ex.init(mockPixiApp)
+})
 
-test("getTexture", (t) => {
+test('getTexture', (t) => {
   //@ts-expect-error
-  t.deepEqual(ex.getTexture("sprite1"), {});
-});
+  t.deepEqual(ex.getTexture('sprite1'), {})
+})
 
-test("getTexture - texture not found", (t) => {
-  t.throws(() => ex.getTexture("sprite2"));
-});
+test('getTexture - texture not found', (t) => {
+  t.throws(() => ex.getTexture('sprite2'))
+})
 
-test("getAllTextureIds", (t) => {
-  t.deepEqual(ex.getAllTextureIds(), ["sprite1"]);
-});
+test('getAllTextureIds', (t) => {
+  t.deepEqual(ex.getAllTextureIds(), ['sprite1'])
+})
 
-test("getAllChildren", (t) => {
+test('getAllChildren', (t) => {
   t.deepEqual(
     ex.getAllChildren(mockStage).map((child) => child.name),
-    [grandChild.name, child1.name, child2.name, container.name, mockStage.name]
-  );
-});
+    [grandChild.name, child1.name, child2.name, container.name, mockStage.name],
+  )
+})
 
-test("resize", (t) => {
+test('resize', (t) => {
   t.notThrows(() => {
-    ex.resize(1200, 1000);
-  });
-});
+    ex.resize(1200, 1000)
+  })
+})
 
-test("showGrid", (t) => {
+test('showGrid', (t) => {
   t.notThrows(() => {
-    const graphics = new PIXI.Graphics();
+    const graphics = new Graphics()
     const gridGraphics = ex.showGrid({
       numberOfCells: 2,
       graphics,
       color: 0xffffff,
-    });
-  });
-});
+    })
+  })
+})
 
 const expectedCells = [
   { x: 0, y: 0 },
   { x: 0, y: 125 },
   { x: 250, y: 0 },
   { x: 250, y: 125 },
-].map((cell) => ({ ...cell, width: 250, height: 125 }));
+].map((cell) => ({ ...cell, width: 250, height: 125 }))
 
-test("getCells - resolution === 1, scale === 2", (t) => {
+test('getCells - resolution === 1, scale === 2', (t) => {
   const cells = internal.getCells({
     width: 1000,
     height: 500,
     numberOfCells: 2,
     resolution: 1,
     scale: 2,
-  });
+  })
 
-  t.deepEqual(cells, expectedCells);
-});
+  t.deepEqual(cells, expectedCells)
+})
 
-test("getCells - resolution === 2, , scale === 1", (t) => {
+test('getCells - resolution === 2, , scale === 1', (t) => {
   const cells = internal.getCells({
     width: 1000,
     height: 500,
     numberOfCells: 2,
     resolution: 2,
     scale: 1,
-  });
+  })
 
-  t.deepEqual(cells, expectedCells);
-});
+  t.deepEqual(cells, expectedCells)
+})
 
-test("centerX", (t) => {
-  const displayObject = { width: 100, x: 0, pivot: { x: 0 } };
-  const xPosition = 500;
+test('centerX', (t) => {
+  const displayObject = { width: 100, x: 0, pivot: { x: 0 } }
+  const xPosition = 500
   //@ts-expect-error
-  ex.centerX(displayObject, xPosition);
-  t.is(displayObject.x, 500);
-  t.is(displayObject.pivot.x, 50);
-});
+  ex.centerX(displayObject, xPosition)
+  t.is(displayObject.x, 500)
+  t.is(displayObject.pivot.x, 50)
+})
 
-test("centerY", (t) => {
-  const displayObject = { height: 100, y: 0, pivot: { y: 0 } };
-  const yPosition = 500;
+test('centerY', (t) => {
+  const displayObject = { height: 100, y: 0, pivot: { y: 0 } }
+  const yPosition = 500
   //@ts-expect-error
-  ex.centerY(displayObject, yPosition);
-  t.is(displayObject.y, 500);
-  t.is(displayObject.pivot.y, 50);
-});
+  ex.centerY(displayObject, yPosition)
+  t.is(displayObject.y, 500)
+  t.is(displayObject.pivot.y, 50)
+})
 
-test("init - duplicate texture names across sprite sheets", (t) => {
+test('init - duplicate texture names across sprite sheets', (t) => {
   //@ts-expect-error
-  t.deepEqual(ex.getTexture("sprite1"), {});
+  t.deepEqual(ex.getTexture('sprite1'), {})
   t.throws(() =>
     ex.init({
       ...mockPixiApp,
@@ -143,16 +143,16 @@ test("init - duplicate texture names across sprite sheets", (t) => {
         ...mockPixiApp.loader,
         resources: {
           ...mockPixiApp.loader.resources,
-          "spritesheet.json": {
+          'spritesheet.json': {
             //@ts-expect-error
-            textures: { "sprite1.png": {} },
+            textures: { 'sprite1.png': {} },
           },
-          "spritesheet2.json": {
+          'spritesheet2.json': {
             //@ts-expect-error
-            textures: { "sprite1.png": {} },
+            textures: { 'sprite1.png': {} },
           },
         },
       },
-    })
-  );
-});
+    }),
+  )
+})
